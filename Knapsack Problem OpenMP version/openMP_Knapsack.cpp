decf53481f15c1bed9e96076f6dbabe597150cd3
Omi Mueller <emorelleum@gmail.com>
2013-07-19 14:42:56 -0400

    OpenMP knapsack Problem
    Removed the previous files

--- Knapsack Problem OpenMP version/openMP_Knapsack.cpp
+++ Knapsack Problem OpenMP version/openMP_Knapsack.cpp
@@ -1,219 +1,324 @@
-/*
- * Michael Crawford
- * David Toth, Ph.D.
- * CPSC 370W
- * The Knapsack Problem, openMP version
- *
- * Command line arguments:
- * argv[1] = test file name 
- */
 
-#include <iostream>
-#include <time.h> 
-#include <fstream>
-#include <algorithm>
+#include<iostream>
+#include<fstream>
+#include<string.h>
+#include<sstream>//wherever a stringstream pops up, it is to convert a multi-digit number to a string or vice versa
+#include"math.h"
+#include<stdlib.h>
 #include <omp.h>
+#include<time.h>
+
+#include<sys/time.h> // Used for timing this.
+#include<unistd.h> // Used for timing this.
+
+#include<algorithm>//needed for next_permutation
+#include<climits>//needed for MAX_INT
+const int MAX_PATH = 1000;//the highest cost of a path in the graph
+void convertFact(unsigned long long, int*);
+void orderPermutation(int*, unsigned long long, int);
+unsigned long long factorial(unsigned long long);
 
 using namespace std;
 
-/* A simple factorial function
- */
-long fact(int i) 
-{
-	return i == 0 ? 1 : i*fact(i-1); 
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+template<class _Ty1, class _Ty2> inline
+	bool prayer(const _Ty1& _Left, const _Ty2& _Right)
+{	// test if _Left < _Right and operator< is strict weak ordering
+	//	cout <<"!!" << endl;
+	if (!(_Left < _Right))
+		return (false);
+	else if (_Right < _Left)
+		cout << "help us" << endl;
+	//_DEBUG_ERROR2("invalid operator<", _File, _Line);
+	return (true);
 }
 
-/* Determines the factoratic at an array location given a permutation number
- */
-void gen(long dec, int pos, int *ret1, long *ret2) 
+inline void swap(int* a, int* b)
 {
-	long this_fact = fact(pos);
-	for(int i=pos; i>-1; i--) 
+	//cout <<"swap!!" << endl;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+inline void reverse(int* a, int* b)
+{
+	//cout <<"reverse!!" << endl;
+	b--;
+	while(a < b)
 	{
-		long fact = this_fact * i;
-		if(fact < dec) 
-		{
-			*ret1 = i;
-			*ret2 = (dec - fact); 
-			return; 
+		swap(a,b);
+		a++;
+		b--;
+		//cout << "swapping: " << *a << " with " << *b << endl;
+	}
+}
+
+inline bool nextPerm(int* _First, int* _Last)
+{	// permute and test for pure ascending, using operator<
+	int* _Next = _Last;
+	if (_First == _Last || _First == --_Next)
+		return (false);
+
+	for (; ; )
+	{	// find rightmost element smaller than successor
+		//	cout <<"!!" << endl;
+		int* _Next1 = _Next;
+		if (prayer(*--_Next, *_Next1))
+		{	// swap with rightmost element that's smaller, flip suffix
+			int* _Mid = _Last;
+			for (; !prayer(*_Next, *--_Mid); )
+				;
+			swap(_Next, _Mid);
+			reverse(_Next1, _Last);
+			return (true);
+		}
+
+		if (_Next == _First)
+		{	// pure descending, flip all
+			reverse(_First, _Last);
+			return (false);
 		}
 	}
 }
+/////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
 
-/* Determines the factoratic array representation of a given permutation number to
- * be used by an individual thread so it can determine which segment of the total
- * permutations it is responsible for calculating.
- */
-void getFactoradic(long dec, long len, int *arr) 
+
+
+
+//returns the factorial for the given number
+//x: the index for which factorial number you want.
+//returns: x!
+unsigned long long factorial(unsigned long long x)
 {
-	int next_digit, j=0; 
-	long next_num, num=dec;
-	for(int i=len-1; i>-1; i--) 
-	{
-		gen(num, i, &next_digit, &next_num); 
-		arr[j++] = next_digit;  
-		num = next_num;  
-	}
+	if(x == 1)
+		return 1;
+	else
+		return x * factorial(x-1);
 }
+//converts a number into base factorial
+//num: the number to convert to base factorial
+//digits: a storage array to store the digits of the base factorial number
+//numbers are stored in reverse order (so the 2nd digit is in position 1, the third in 2, etc.
+//digits[0] will contain the length of the number in digits, since the first number of a base factorial number is always 0
+void convertFact(unsigned long long num, int* digits)
+{
+	int numDigits = 1;//there is always a spare 0 in these numbers
+	while(num > 0)
+	{
+		digits[numDigits] = num % (numDigits + 1);
+		numDigits++;
+		num /= numDigits;
+	}
+	digits[0] = numDigits;//because the first digit is always zero, we will store the length of the array in the 0th slot
+}
+//returns the array transposed to the nth permutation after the given ordering
+//toOrder: the set for which you would like to define the nth permutation
+//m: the numbered permutation to be stored in toOrder
+//size: the size of the array toOrder
+//warning: gives unpredictable behavior if n is > the number of unique permutations for toOrder
+void orderPermutation(int* toOrder, unsigned long long n, int size)
+{
+	int swaps[100];
+	convertFact(n, swaps);
+	int location = size - swaps[0];//accounts for leading zeros
+	int loc = swaps[0] - 1;//used to iterate through the digits of the factoradic number
+	while(loc > 0)
+	{
+		int temp = toOrder[location + swaps[loc]];
+		for(int x = location+swaps[loc]; x > location; x--)//moves all the other numbers up
+		{
+			toOrder[x] = toOrder[x - 1];
+		}
+		toOrder[location] = temp;
+		location++;
+		loc--;
+	}
+
+}
+
 
 int main(int argc, char* argv[])
 {
-	ifstream file (argv[1]);
-	int numTrials;
-	file >> numTrials;
 
-	for (int j=0; j<numTrials; j++)
+	struct timeval startTime, endTime;
+	gettimeofday(&startTime, NULL);
+
+	if(argc != 4)
 	{
- 		clock_t start;
-		clock_t end;
+		cout<<"You have enetered and invalid argument list"<<endl;
+		cout<<"Required(number of Items) (seed) (number of threads)"<<endl;
+	}
 
-		start = clock();
+	stringstream arg1(argv[1]);
+	int numItems;
+	arg1 >> numItems;
 
-	  	int maxWeight, numItems, usedWeight;  
-	    // initialize arrays to numItems
-	    file >> numItems;
-	    file >> maxWeight;
+	stringstream arg2(argv[2]);
+	int randomSeed;
+	arg2 >> randomSeed;
+
+	srand(randomSeed);
+
+	stringstream arg3(argv[3]);
+	int numThreads;
+	arg3 >> numThreads;
+	if(numThreads == 0)
+	{
+		numThreads = omp_get_max_threads();
+	}
+
+	int maxWeight = (numItems *10) / 2;
+
+
+
+	// initialize arrays to numItems
+	//cout<<numItems<<endl;
+	//cout<<maxWeight<<endl;
 
 	    // take in this round's values and weights
-	   	int values[numItems], weights[numItems];
-	    for (int i = 0; i < numItems; i++) file >> values[i];
-	    for (int i = 0; i < numItems; i++) file >> weights[i];
-	    
-		int index[numItems]; // because one must get all the permutations on the indices
-		for (int i=0; i<numItems; i++) index[i] = i;
+	int* values = new int[numItems];
+	int* weights = new int[numItems];
 
-		int bestItems[numItems];
-		for (int i=0; i<numItems; i++) bestItems[i] = -1;
+	for(int index = 0; index < numItems; index++)
+	{
+		values[index] = rand() % 30;
+		//cout<<values[index]<<" ";
+	}
+	//cout<<endl;
+	for(int index = 0; index < numItems; index++)
+	{
+		weights[index] = rand() % 30;
+		//cout<<weights[index]<<" ";
+	}
+	//cout<<endl;
 
-		int maxValueSoFar = 0;
 
-		int overallMaxValue = 0;
-		int overallUsedWeight = 0;
-		int overallBestItems[numItems];
 
-		int threadCount;
-		long chunkSize;
 	
-		#pragma omp parallel firstprivate(index, bestItems, maxValueSoFar, usedWeight) shared(overallMaxValue, overallUsedWeight, overallBestItems)
+	long long* permStartIndecies = new long long[numThreads + 1];
+
+	long long numPerms = factorial(numItems);
+
+	double sectionRatio = double(1)/numThreads;
+
+	for(int index = 0; index < numThreads + 1; index++)
+	{
+		permStartIndecies[index] = numPerms * (sectionRatio * index);
+	}
+
+	long long* bestPermArray = new long long[numThreads];
+	int* bestValueArray = new int[numThreads];
+
+
+	////entering the parallel section
+	#pragma omp parallel num_threads(numThreads)
+	{
+		int bestValue = 0;
+		long long bestPerm = LLONG_MAX;
+
+		int Thread_ID = omp_get_thread_num();
+		int totSum;
+		int totWeight;
+
+		int* itemPermArray = new int[numItems];
+		for(int index = 0; index < numItems; index++)
 		{
-			int tid = omp_get_thread_num();
-			threadCount = omp_get_num_threads();
-			chunkSize = fact(numItems)/threadCount;
-			long startPermutation = (chunkSize * tid) + 1;
-			long counter = 0;
-
-			int *indicies = new int[numItems];
-			int *nums = new int[numItems];
-			for(int i = 0;i < numItems;i++) 
-				nums[i] = i;
-			
-			getFactoradic(startPermutation, numItems, indicies);
-
-			/* turn the factoradic into a permutation */
-			for(int i=0; i<numItems; i++) 
-			{
-				index[i] = nums[indicies[i]];
-				for(int j=indicies[i]; j<numItems; j++) 
-					nums[j] = nums[j+1]; 
-				nums[numItems-1] = 0; 
-			} 
-
-			/* testing permutation generation */
-			#pragma omp critical(print)
-			{
-				cout << "Thread: " << tid << "; permutation number: " << startPermutation << "; startPermutation: ";
-				cout << "[";
-				for (int i = 0; i < numItems; i++)
-					cout << index[i] << ",";
-				cout << "]" << endl;
-			}
-
-			do 
-			{
-				int curWeight = 0;
-				int curValue = 0;
-				int thisRoundItems[numItems];
-				for (int i=0; i<numItems; i++) thisRoundItems[i] = -1;
-
-				for (int i = 0; i < numItems; i++)
-				{
-					if (curWeight + weights[index[i]] <= maxWeight)
-					{
-						curWeight += weights[index[i]];
-						curValue += values[index[i]];
-						thisRoundItems[i] = index[i];
-					}
-					else break;
-				}
-
-				if (curValue > maxValueSoFar) 
-				{
-					maxValueSoFar = curValue;	
-					usedWeight = curWeight;
-					for (int i = 0; i < numItems; i++)
-					{
-						bestItems[i] = thisRoundItems[i];
-					}
-				}
-			} while (next_permutation(index, index+numItems) && counter++ < chunkSize);
-
-			#pragma omp critical(maxValue)
-			{
-				if (maxValueSoFar > overallMaxValue)
-				{
-					overallMaxValue = maxValueSoFar;
-					overallUsedWeight = usedWeight;
-					for (int i = 0; i < numItems; i++)
-					{
-						overallBestItems[i] = bestItems[i];
-					}
-				}
-			}
-
-			#pragma omp critical(print)
-			{
-				cout << "TID: " << tid << "'s best is: " << maxValueSoFar << " and ends at: ";
-				cout << "[";
-				for (int i=0; i<numItems; i++) 
-					cout << index[i] << ",";
-				cout << "], with a counter of " << counter << endl;
-			}
+			itemPermArray[index] = index;
 		}
 
-		end = clock();
+		orderPermutation(itemPermArray,permStartIndecies[Thread_ID],numItems);
+
+
+		for(long long index = permStartIndecies[Thread_ID]; index < (permStartIndecies[Thread_ID + 1]); index++)
+		{
+			
+			totSum = 0;
+			totWeight = 0;
+			int i = 0;
+			for(int w = 0; w < numItems; w++)
+			{
+
+				totWeight = totWeight + weights[itemPermArray[w]];
+				if(totWeight > maxWeight)
+				{
+					break;
+				}
+				totSum = totSum + values[itemPermArray[w]];
+				
+				i++;
+			}
+
+			
+			if(totSum > bestValue)
+			{
+				bestValue = totSum;
+				bestPerm = index;
 		
-		/** 
-		 * Printing
-		 */
+			}
+
 		
-		cout << "Trial number: " << j+1 << endl;
-		cout << "Number of items: " << numItems << endl;
-		cout << "Max capacity: " << maxWeight << endl;
-		cout << "Max permutations: " << fact(numItems) << endl;
-		cout << "Worker chunk size: " << chunkSize << endl;
-		cout << "It took " << ((float)(end-start))/CLOCKS_PER_SEC << " seconds"  << endl;
+			nextPerm(itemPermArray, itemPermArray + numItems);
+			
+		
+		}
 
-	  	cout << "Values:  ";
-	    for (int i=0; i<numItems; i++) cout << values[i] << " ";
-	  	cout << endl;
-
-	  	cout << "Weights: ";
-	  	for (int i=0; i<numItems; i++) cout << weights[i] << " ";
-	  	cout << endl;
-
-	  	cout << "Indices: ";
-	  	for (int i=0; i<numItems; i++) cout << i << " ";
-	  	cout << endl;
-
-		cout << "Overall best items: ";
-		for (int i=0; i<numItems; i++) 
-			if (overallBestItems[i] != -1) 
-				cout << overallBestItems[i] << " ";
-		cout << endl;
-
-		cout << "Weight used: " << overallUsedWeight << endl;
-		cout << "Total value: " << overallMaxValue << endl << endl;
+		bestValueArray[Thread_ID] = bestValue;
+		bestPermArray[Thread_ID] = bestPerm;
 
 	}
-	return 0;
+
+	int finalValue;
+	unsigned long long finalPerm = 0;
+	int tempValue = 0;
+	for(int index = 0; index < numThreads; index++)
+	{
+		if(bestValueArray[index] > tempValue)
+		{
+			tempValue = bestValueArray[index];
+			finalPerm = bestPermArray[index];
+		}
+	}
+	finalValue = tempValue;
+
+	int* permArray = new int[numItems];
+	for(int index = 0; index < numItems; index++)
+	{
+		permArray[index] = index;
+	}
+
+	/*orderPermutation(permArray, finalPerm, numItems);
+	
+	int checkWeight = 0;
+	for(int index = 0; index < numItems; index++)
+	{
+		checkWeight = checkWeight + weights[permArray[index]];
+		if(checkWeight > maxWeight)
+		{
+			break;
+		}
+		cout <<  permArray[index]<< ", ";
+	}
+
+	cout<< "best value for theft: "<< finalValue<<endl;
+	*/
+	
+
+	gettimeofday(&endTime, NULL);
+	long timeDelta, startSeconds, startUSeconds, stopSeconds, stopUSeconds, startTotal, stopTotal;
+	startSeconds = startTime.tv_sec;
+	stopSeconds = endTime.tv_sec;
+	startUSeconds = startTime.tv_usec;
+	stopUSeconds = endTime.tv_usec;
+	startTotal = (startSeconds * 1000) + (startUSeconds / 1000);
+	stopTotal = (stopSeconds * 1000) + (stopUSeconds / 1000);
+
+
+	timeDelta = stopTotal - startTotal;
+	cout << "Time: " << timeDelta << " milliseconds" << endl;
+	
+
+	
 }
